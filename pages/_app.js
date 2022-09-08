27226f4dca2d79726fede93806b5a5b10af84d08
josephedward <josephedwardwork@gmail.com>
2022-09-08 00:31:22 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,25 @@
 import '../styles/globals.css'
+//no logs in prod
+if (process.env.NODE_ENV === "production") {
+  function noop() {}
+  const savedFunctions = Object.keys(console).reduce((memo, key) => {
+    if (typeof console[key] == "function") {
+      //keep a copy just in case we need it
+      memo[key] = console[key];
+      //de-fang any functions
+      console[key] = noop;
+    }
+    return memo;
+  }, {});
+  function logTest() {
+    console.log("log");
+    console.info("info");
+    console.warn("warn");
+    console.error("error");
+    savedFunctions.log("logging from saved function");
+  }
+  logTest();
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
